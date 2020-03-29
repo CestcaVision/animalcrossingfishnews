1e8ecf899fa7b0e811974a1337b9ac53b539f4d3
Tricrepe <644568681@qq.com>
2020-03-29 21:29:25 +0800

    鱼情初步完成

---
+++ precache-manifest.9a0ed6e0c67f46516d4799bbd3096c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc5f2d3886c5facb69863820f8c441e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4fd64ba5eb7e72fe957",
+    "url": "/static/css/main.2e076cb5.chunk.css"
+  },
+  {
+    "revision": "9277ad0587560da4d2dd",
+    "url": "/static/js/2.261892c4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.261892c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4fd64ba5eb7e72fe957",
+    "url": "/static/js/main.c1ffb440.chunk.js"
+  },
+  {
+    "revision": "6f10e21a1bc2cd168844",
+    "url": "/static/js/runtime-main.5802c77a.js"
+  },
+  {
+    "revision": "3ebb2a0429862d6e63b7632c90d9fadf",
+    "url": "/static/media/github.3ebb2a04.svg"
+  }
+]);
